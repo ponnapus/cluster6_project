a763ee2fda8b8e4f4b6ca88e24cd5f86840e7424
ponnapus <72852827+ponnapus@users.noreply.github.com>
2020-10-19 07:12:59 +0700

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -1,21 +1,37 @@
-#ifndef CUSTOMER_H
-#define CUSTOMER_H
-#include <string>
+#include "customer.h"
+#include <iostream>
 
 using namespace std;
-class customer
+customer::customer()
 {
-	public:
-		string username,name,lname;
-		string num_people;
-		string day_in;
-		string day_out;
-		int num_booking;
-		string num_room;
-		string tel;
-		customer();
-		void Booking();
-	protected:
-};
+}
 
-#endif
+void customer::Booking(){
+			cout << "Enter Num People : ";
+			cin >> num_people;
+			cout << "Enter day in : ";
+			cin >> day_in;
+			cout << "Enter day out : ";
+			cin >> day_out;
+			//	data();
+			cout << "Booking"<< endl;
+			cout << "1. Booking" << endl;
+			cout << "2. return home" << endl;
+			cout << "=================" << endl;
+			cout << "Enter : ";
+				cin >> num_booking;
+			if(num_booking == 1){
+			
+				cout << "+++++++++++++++++++ BOOKING ++++++++++++++++++++" << endl;
+				cout << "Enter number room : ";
+					cin >> num_room;
+				cout << "Enter Name and lastname ";
+					cin >> name >> lname;
+				cout << "num tel" ;
+					cin >> tel;
+				cout << "================================================" << endl;
+			}else{
+				
+			}	
+
+}
