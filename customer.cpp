363915912c27954218ad1638ec3de0bcbc776220
ponnapus <72852827+ponnapus@users.noreply.github.com>
2020-10-19 07:11:35 +0700

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -1,7 +1,21 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include <string>
 
-#include "customer.h"
-
-customer::customer()
+using namespace std;
+class customer
 {
-}
+	public:
+		string username,name,lname;
+		string num_people;
+		string day_in;
+		string day_out;
+		int num_booking;
+		string num_room;
+		string tel;
+		customer();
+		void Booking();
+	protected:
+};
 
+#endif
